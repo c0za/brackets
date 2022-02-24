38300a9a20881734ddf788c7d287afae6436d31b
Andrey Maximov <c0za@mail.ru>
2022-02-24 20:27:47 +0300

    feat: finish brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+  const BRACKETS_PAIR = {
+    [')']: '(',
+    ['}']: '{',
+  };
+
+    let stack = [];
+
+    for (let i = 0; i < str.length; i++) {
+      let currentSymbol = str[i];
+      
+      if (bracketsConfig.includes(currentSymbol)) {
+        stack.push(currentSymbol);
+      } else {
+        if (stack.length === 0) {
+          return false;
+        }
+
+        let topElement = stack[stack.length - 1];
+
+        if (BRACKETS_PAIR[currentSymbol] === topElement) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+
+    return stack.length === 0;
+  }
